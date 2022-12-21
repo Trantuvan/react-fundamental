858fc441a5b392ef090309567df5c688203002d6
trantuvan <trantuvan.kan@gmail.com>
2022-12-21 11:15:38 +0700

    recives components param from obj props

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Greet = (props) => {
-  return <h1 className="greet">Hello Van</h1>;
+  return (
+    <h1 className="greet">
+      Hello {props.name} a.k.a {props.heroName}
+    </h1>
+  );
 };
 
 export default Greet;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 
 export default class Welcome extends Component {
   render() {
-    return <h1>Welcome Van</h1>;
+    return (
+      <h1>
+        Welcome {this.props.name} a.k.a {this.props.heroName}
+      </h1>
+    );
   }
 }
